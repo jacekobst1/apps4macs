5249e092e686318721ac025bfc1d166fd52d66db
jacek.obst <jacek.obst@emoti.group>
2024-07-22 19:58:07 +0200

    feat: create BaseLabel and add input variants

--- resources/js/Components/form/BaseInput.vue
+++ resources/js/Components/form/BaseInput.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
     import {computed} from "vue";
+    import BaseLabel from "@/Components/form/BaseLabel.vue";
 
     interface Props {
+        variant?: "default" | "primary";
         size?: 'xs' | 'sm' | 'md' | 'lg';
         placeholder?: string;
+        label?: string;
     }
 
     const props = withDefaults(defineProps<Props>(), {
+        variant: 'default',
         size: 'md',
         placeholder: '',
+        label: '',
     });
 
     const model = defineModel()
@@ -28,14 +33,33 @@
         }
     });
 
+    const inputClasses = computed(() => [
+        ['input w-full rounded-xl', sizeClass.value],
+        [
+            {'': props.variant === "default"},
+            {'input-primary': props.variant === "primary"},
+        ]
+    ]);
 </script>
 
 <template>
-    <input
-        class="input rounded-xl"
-        :class="sizeClass"
-        v-model="model"
-        v-bind="$attrs"
-        :placeholder="props.placeholder"
-    />
+    <template v-if="label">
+        <BaseLabel :text="label">
+            <input
+                v-model="model"
+                v-bind="$attrs"
+                :class="inputClasses"
+                :placeholder="placeholder"
+            />
+        </BaseLabel>
+    </template>
+
+    <template v-else>
+        <input
+            v-model="model"
+            v-bind="$attrs"
+            :class="inputClasses"
+            :placeholder="placeholder"
+        />
+    </template>
 </template>
---
+++ resources/js/Components/form/BaseLabel.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    const props = defineProps<{ text: string }>();
+
+    defineOptions({
+        inheritAttrs: false,
+    })
+</script>
+
+<template>
+    <label class="block">
+    <span class="block text-xs md:text-sm font-medium select-none text-base-content px-1 mb-0.5">
+      {{ props.text }}
+    </span>
+        <slot/>
+    </label>
+</template>
