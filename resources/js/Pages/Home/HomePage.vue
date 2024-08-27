1c2d1d3739d50dff854129b64739207d3a94bb91
Jacek Obst <jacekobst1@gmail.com>
2024-08-27 18:24:17 +0200

    fix: duplicated apps when there are less than one page

--- resources/js/Pages/Home/HomePage.vue
+++ resources/js/Pages/Home/HomePage.vue
@@ -17,10 +17,17 @@
     const last = ref(null);
     const noMoreItems = ref(false);
     const isLoading = ref(false);
+    const allApps = ref(props.apps.data);
 
     const loadData = () => {
+        if (!props.apps.next_cursor) {
+            noMoreItems.value = true;
+            isLoading.value = false;
+            return;
+        }
+
         axios.get(`${props.apps.path}?cursor=${props.apps.next_cursor}`).then((response: any) => {
-            props.apps.data = [...props.apps.data, ...response.data.data];
+            allApps.value = [...allApps.value, ...response.data.data];
             props.apps.next_cursor = response.data.next_cursor;
             props.apps.next_page_url = response.data.next_page_url;
             props.apps.path = response.data.path;
@@ -33,17 +40,20 @@
                 noMoreItems.value = true;
                 stop();
             }
-        })
+        }).catch(() => {
+            isLoading.value = false;
+        });
     };
 
     const {stop} = useIntersectionObserver(
         last,
         ([{isIntersecting}]) => {
-            if (isIntersecting) {
+            if (isIntersecting && !isLoading.value && !noMoreItems.value) {
                 isLoading.value = true;
                 setTimeout(loadData, 250)
             }
-        }
+        },
+        {threshold: 0.5}
     );
 
     const submitAppLink = computed(() => props.auth.user ? '/new-app/specify-if-paid' : '/sign-up');
@@ -54,7 +64,6 @@
 
     <StandardLayout>
         <div class="mb-8 pt-6">
-            <!--            <ApplicationLogo width="150px" height="150px"/>-->
             <div class="text-sm sm:text-lg mb-4 text-gray-500">
                 <p>No-brainer apps for devs and powerusers</p>
                 <p>Verified and maintained by "HumanIntelligence"</p>
@@ -72,12 +81,12 @@
         </div>
 
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 xl:gap-6 mb-10">
-            <template v-for="app in apps.data" :key="app.id">
+            <template v-for="app in allApps" :key="app.id">
                 <AppTile :app="app" class="justify-self-center"/>
             </template>
         </div>
-        <div ref="last" class="-translate-y-32"></div>
-        <div v-if="isLoading" class="mb-10 text-center font-bold">
+        <div v-if="!noMoreItems" ref="last" class="h-10"></div>
+        <div v-if="isLoading && !noMoreItems" class="mb-10 text-center font-bold">
             <span>Loading more items...</span>
         </div>
     </StandardLayout>
