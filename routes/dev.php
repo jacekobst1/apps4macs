442799f5255583219137ca577677c04ed13ad0cf
Jacek Obst <jacekobst1@gmail.com>
2024-08-27 17:45:58 +0200

    chore: protect dev login with ip check

--- routes/dev.php
+++ routes/dev.php
@@ -7,7 +7,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
-if (!app()->isProduction()) {
+$envMatch = !app()->isProduction();
+$ipMatch = in_array(request()->ip(), ['127.0.0.1', Config::get('env.admin_ip')]);
+
+if ($envMatch && $ipMatch) {
     Route::get('/dev/login', function () {
         $adminEmail = Config::get('env.admin_email');
         $user = User::whereEmail($adminEmail)->first();
