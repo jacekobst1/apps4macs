3dbd54e600ca33e771551b35394bab80f4b6f3d5
Jacek Obst <jacekobst1@gmail.com>
2024-09-09 20:03:13 +0200

    feat: change description on the homepage

--- resources/js/Pages/Home/HomePage.vue
+++ resources/js/Pages/Home/HomePage.vue
@@ -64,8 +64,8 @@
     <StandardLayout>
         <div class="mb-8 pt-6">
             <div class="text-sm sm:text-lg mb-4 text-gray-500">
-                <p>No-brainer apps for devs and powerusers</p>
-                <p>Verified and maintained by "HumanIntelligence"</p>
+                <p>The coolest apps for devs and other geeks</p>
+                <p>Maintained by humans. No trash</p>
             </div>
             <Link :href="submitAppLink">
                 <BaseButton class="shadow-2xl">
